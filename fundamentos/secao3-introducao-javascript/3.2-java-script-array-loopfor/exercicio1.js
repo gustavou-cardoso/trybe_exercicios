0625fe87cf3f73f2913a43be586b1de5356732a6
Gustavo Urgel Cardoso <gugazinhourgel@hotmail.com>
2023-01-24 18:39:22 -0300

    feito exercicio 4

--- fundamentos/secao3-introducao-javascript/3.2-java-script-array-loopfor/exercicio1.js
+++ fundamentos/secao3-introducao-javascript/3.2-java-script-array-loopfor/exercicio1.js
@@ -1,8 +1,28 @@
+
+// Exercício 1
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]; 
 console.log(numbers)
 
+// Exercício 2
 let result = 0 
     for (let index = 0; index < numbers.length; index++) {
     result += numbers[index];
     }
 console.log(result)
+
+//Exercício 3
+let soma = 0;
+for (let index = 0; index < numbers.length; index++) {
+    soma += numbers[index];
+}
+let divisao = soma / numbers.length;
+
+console.log(divisao);
+
+// Exercicio 4
+if (divisao > 20) {
+console.log("valor maior que 20");
+}
+else  {
+    console.log("valor menor que 20");
+}
